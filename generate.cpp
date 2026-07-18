002eafd4d60a1550a5deaecf97ac56fbff5645aa
ruslan <bust6k.gh@gmail.com>
2026-07-18 14:23:04 +0300

    just backup

--- generate.cpp
+++ generate.cpp
@@ -903,7 +903,7 @@ void generate_varip_buffer(std::ofstream& output, int indent) {
 
 int main(int argc, char* argv[]) {
   Pinefan::Ppp::preprocess_files(argc, argv);
-  yydebug = 1;
+  //yydebug = 1;
   for (int i = 0; i < Pinefan::File::preprocessed_files.size(); i++) {
     Pinefan::File::Prp_file* prped_file =
         Pinefan::File::preprocessed_files.at(i);
